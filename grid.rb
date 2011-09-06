32738c561b09a70f311527a6c851797f930933ea
Paul Vinieratos <pvinis@gmail.com>
2011-09-07 01:57:31 +0300

    made a simple test grid

--- grid.rb
+++ grid.rb
@@ -1,16 +1,31 @@
+require './tile'
 
 class Grid
-  def initialize(level)
-    @level = level
+  def initialize()
+    @tiles = []
     ##read_level
     ##create tiles
     ###...
   end
   
-  def test_grid
+  def test_grid 
     @tiles = []
+    line = []
+    line << Tile.new(0, 0, Visible::YES, Visible::YES, Visible::YES, Visible::YES)
+    line << Tile.new(1, 0, Visible::YES, Visible::YES, Visible::YES, Visible::NO)
+    @tiles << line
+    line = []
+    line << Tile.new(0, 1, Visible::YES, Visible::YES, Visible::YES, Visible::YES)
+    line << Tile.new(1, 1, Visible::YES, Visible::YES, Visible::YES, Visible::YES)
+    @tiles << line
+    return self
+  end
+
+  def draw
+    @tiles.each do |line|
+      line.each do |tile|
+        tile.draw
+      end
+    end
   end
 end
-
-
-
